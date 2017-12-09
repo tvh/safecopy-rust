7fed43ba0c9cbec29af60ef7ff6dd3c47a839b03
Timo von Holtz <vonholtz@cp-med.com>
2017-12-09 23:44:47 +0100

    Switch to bincode

--- src/lib.rs
+++ src/lib.rs
@@ -1,86 +1,65 @@
-#[macro_use]
-extern crate nom;
+extern crate bincode;
 
-use nom::{IResult, le_i32, le_i64};
+use bincode::*;
 use std::marker::{Sized};
 use std::convert::{From};
+use std::io::{Read, Write};
+use std::boxed::Box;
 
 trait Kind<A: SafeCopy> {
-    fn safe_parse(i: &[u8]) -> IResult<&[u8], A>;
+    fn safe_parse<R: Read>(reader: &mut R) -> Result<A>;
+    fn safe_parse_versioned<R: Read>(v: i32, reader: &mut R) -> Result<A>;
 }
 
 pub struct Primitive;
 impl<A: SafeCopy> Kind<A> for Primitive {
-    named!(safe_parse<A>, call!(A::parse_unsafe));
+    fn safe_parse<R: Read>(reader: &mut R) -> Result<A> {
+        A::parse_unsafe(reader)
+    }
+
+    fn safe_parse_versioned<R: Read>(_v: i32, _reader: &mut R) -> Result<A> {
+        Err(Box::new(ErrorKind::Custom(String::from("Migration with Primitive"))))
+    }
 }
 
 pub struct Base;
 impl<A: SafeCopy> Kind<A> for Base {
-    named!(safe_parse<A>, do_parse!(
-        v: le_i32 >>
-        res: cond_reduce!(v==A::VERSION, A::parse_unsafe) >>
-        (res)
-    ));
-}
+    fn safe_parse<R: Read>(reader: &mut R) -> Result<A> {
+        deserialize_from(reader, Infinite)
+            .and_then(|v: i32| {
+                Self::safe_parse_versioned(v, reader)
+            })
+    }
 
-pub struct Extended<B>(::std::marker::PhantomData<B>);
-impl<A: SafeCopy+From<B>, B: SafeCopy> Kind<A> for Extended<B> {
-    named!(safe_parse<A>, alt!(
-        do_parse!(
-            v: le_i32 >>
-            res: cond_reduce!(v==A::VERSION, A::parse_unsafe) >>
-            (res)
-        ) |
-        call!(A::K::safe_parse)
-    ));
-}
-
-trait SafeCopy: Sized {
-    type K: Kind<Self>;
-    fn parse_unsafe(i: &[u8]) -> IResult<&[u8], Self>;
-    const VERSION: i32;
-}
-
-impl SafeCopy for i32 {
-    type K = Primitive;
-    named!(parse_unsafe<i32>, call!(le_i32));
-    const VERSION: i32 = 0;
-}
-
-impl SafeCopy for i64 {
-    type K = Primitive;
-    named!(parse_unsafe<i64>, call!(le_i64));
-    const VERSION: i32 = 0;
-}
-
-
-
-struct Foo(i32);
-
-impl SafeCopy for Foo {
-    type K = Base;
-    named!(parse_unsafe<Foo>, do_parse!(
-        val: le_i32 >>
-        (Foo(val))
-    ));
-    const VERSION: i32 = 0;
-}
-
-struct Bar(i64);
-
-impl From<Foo> for Bar {
-    fn from(x: Foo) -> Bar {
-        match x {
-            Foo(val) => Bar(i64::from(val))
+    fn safe_parse_versioned<R: Read>(v: i32, reader: &mut R) -> Result<A> {
+        if v==A::VERSION {
+            A::parse_unsafe(reader)
+        } else {
+            Err(Box::new(ErrorKind::Custom(String::from("Wrong Version"))))
         }
     }
 }
 
-impl SafeCopy for Bar {
-    type K = Extended<Foo>;
-    named!(parse_unsafe<Bar>, do_parse!(
-        val: le_i64 >>
-        (Bar(val))
-    ));
-    const VERSION: i32 = 1;
+pub struct Extended<B>(::std::marker::PhantomData<B>);
+impl<A: SafeCopy+From<B>, B: SafeCopy> Kind<A> for Extended<B> {
+    fn safe_parse<R: Read>(reader: &mut R) -> Result<A> {
+        deserialize_from(reader, Infinite)
+            .and_then(|v: i32| {
+                Self::safe_parse_versioned(v, reader)
+            })
+    }
+
+    fn safe_parse_versioned<R: Read>(v: i32, reader: &mut R) -> Result<A> {
+        if v==A::VERSION {
+            A::parse_unsafe(reader)
+        } else {
+            B::K::safe_parse_versioned(v, reader).map(From::from)
+        }
+    }
+}
+
+trait SafeCopy: Sized {
+    type K: Kind<Self>;
+    fn parse_unsafe<R: Read>(bytes: &mut R) -> Result<Self>;
+    const VERSION: i32;
 }

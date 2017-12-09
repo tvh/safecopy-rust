848a521257b35e6406000abb94b6b52c758ef603
Timo von Holtz <vonholtz@cp-med.com>
2017-12-09 22:05:48 +0100

    Some more code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate nom;
 
-use nom::{IResult, le_i32};
+use nom::{IResult, le_i32, le_i64};
 use std::marker::{Sized};
 use std::convert::{From};
 
@@ -9,21 +9,26 @@ trait Kind<A: SafeCopy> {
     fn safe_parse(i: &[u8]) -> IResult<&[u8], A>;
 }
 
-struct Base;
+pub struct Primitive;
+impl<A: SafeCopy> Kind<A> for Primitive {
+    named!(safe_parse<A>, call!(A::parse_unsafe));
+}
+
+pub struct Base;
 impl<A: SafeCopy> Kind<A> for Base {
     named!(safe_parse<A>, do_parse!(
         v: le_i32 >>
-        res: cond_reduce!(v==A::version(), A::parse) >>
+        res: cond_reduce!(v==A::VERSION, A::parse_unsafe) >>
         (res)
     ));
 }
 
-struct Extended<B>(::std::marker::PhantomData<B>);
+pub struct Extended<B>(::std::marker::PhantomData<B>);
 impl<A: SafeCopy+From<B>, B: SafeCopy> Kind<A> for Extended<B> {
     named!(safe_parse<A>, alt!(
         do_parse!(
             v: le_i32 >>
-            res: cond_reduce!(v==A::version(), A::parse) >>
+            res: cond_reduce!(v==A::VERSION, A::parse_unsafe) >>
             (res)
         ) |
         call!(A::K::safe_parse)
@@ -32,33 +37,50 @@ impl<A: SafeCopy+From<B>, B: SafeCopy> Kind<A> for Extended<B> {
 
 trait SafeCopy: Sized {
     type K: Kind<Self>;
-    fn parse(i: &[u8]) -> IResult<&[u8], Self>;
-    fn version() -> i32;
+    fn parse_unsafe(i: &[u8]) -> IResult<&[u8], Self>;
+    const VERSION: i32;
 }
 
-struct Foo();
+impl SafeCopy for i32 {
+    type K = Primitive;
+    named!(parse_unsafe<i32>, call!(le_i32));
+    const VERSION: i32 = 0;
+}
+
+impl SafeCopy for i64 {
+    type K = Primitive;
+    named!(parse_unsafe<i64>, call!(le_i64));
+    const VERSION: i32 = 0;
+}
+
+
+
+struct Foo(i32);
 
 impl SafeCopy for Foo {
     type K = Base;
-    named!(parse<Foo>, do_parse!(
-        (Foo())
+    named!(parse_unsafe<Foo>, do_parse!(
+        val: le_i32 >>
+        (Foo(val))
     ));
-    fn version() -> i32 { 0 }
+    const VERSION: i32 = 0;
 }
 
-struct Bar(i32);
+struct Bar(i64);
 
 impl From<Foo> for Bar {
-    fn from(_x: Foo) -> Bar {
-        Bar(0)
+    fn from(x: Foo) -> Bar {
+        match x {
+            Foo(val) => Bar(i64::from(val))
+        }
     }
 }
 
 impl SafeCopy for Bar {
     type K = Extended<Foo>;
-    named!(parse<Bar>, do_parse!(
-        val: le_i32 >>
+    named!(parse_unsafe<Bar>, do_parse!(
+        val: le_i64 >>
         (Bar(val))
     ));
-    fn version() -> i32 { 1 }
+    const VERSION: i32 = 1;
 }
